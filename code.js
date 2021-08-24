aca7f8489aaa12f1b21c44a50423c2e19a5c9eb0
Destiney Rodney <destiney.rodney01@gmail.com>
2021-08-24 15:55:56 -0500

    add the eigth test and solution for intro-to-testing

--- code.js
+++ code.js
@@ -5,15 +5,29 @@ function helloWorld() {
 
 //sayHello
 function sayHello(input) {
-//     if (input === 'Alex'){
-//         return "Hello, Alex!";
-//     } else if (input === 'Pat'){
-//         return "Hello, Pat!";
-//     } else {
-//         return "Hello, Jane!"
-//     }
-return "Hello, " + input + "!";
+    // if (input === 'Alex'){
+    //     return "Hello, Alex!";
+    // } else if (input === 'Pat'){
+    //     return "Hello, Pat!"
+    // } else if (input === 'Jane'){
+    // return "Hello, Jane!";
+    // }
+    if (input == "") {
+        return "Hello, World";
+    } else if (typeof input === 'string'){
+        return "Hello, " + input + "!";
+    } else if (input === false){
+        return "Hello, World!";
+    } else if (input === true) {
+        return "Hello, World!";
+    } else if (input !== 'string'){
+        return "This is not a name!";
+    } else {
+        return "Hello, World!";
+    }
 }
+// return "Hello, " + input + "!";
+
 
 
 
