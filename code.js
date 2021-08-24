daa8fe1bb2ee294010934e8028603a5a2d8bdf55
Destiney Rodney <destiney.rodney01@gmail.com>
2021-08-24 14:29:17 -0500

    add the sixth test and solution for intro-to-testing

--- code.js
+++ code.js
@@ -5,13 +5,14 @@ function helloWorld() {
 
 //sayHello
 function sayHello(input) {
-    if (input === 'Alex'){
-        return "Hello, Alex!";
-    } else if (input === 'Pat'){
-        return "Hello, Pat!";
-    } else {
-        return "Hello, Jane!"
-    }
+//     if (input === 'Alex'){
+//         return "Hello, Alex!";
+//     } else if (input === 'Pat'){
+//         return "Hello, Pat!";
+//     } else {
+//         return "Hello, Jane!"
+//     }
+return "Hello, " + input + "!";
 }
 
 
